5338e548ecbf7c685951cc5a8b3b9feb651134ed
Jivan Sharma <jivan4sharma3@gmail.com>
2025-08-31 12:28:27 +0545

    first commit

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+const connectToMongoo = require("./db")
+const express = require('express')
+connectToMongoo()
+
+const app = express()
+const port = 5000
+
+app.use(express.json())
+
+// Available Routes
+app.use('/api/auth',require('./routes/auth'))
+app.use('/api/notes',require('./routes/notes'))
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`)
+})
+ 
---
+++ backend/models/User.js
@@ -0,0 +1,26 @@
+const { default: userEvent } = require('@testing-library/user-event');
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+
+const UserSchema = new Schema({
+    name:{
+        type: String,
+        require: true
+    },
+    email:{
+        type: String,
+        require: true,
+        unique: true
+    },
+    password:{
+        type: String,
+        require: true
+    },
+    timestamp:{
+        type: Date,
+        default: Date.now
+    },
+});
+const User = mongoose.model("user",UserSchema)
+module.exports = User
