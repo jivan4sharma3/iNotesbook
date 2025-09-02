a3460b1944772d4f7fa30785a90a28cfc1ef1016
Jivan Sharma <jivan4sharma3@gmail.com>
2025-09-02 13:42:40 +0545

    first commit

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -41,4 +41,29 @@ router.post('/addnotes', fetchuser, [
     }
 });
 
+// ROUTE 2 :  Updating an existing  Notes using: POST "/api/notes/updatenote". login required
+router.put('/updatenote/:id', fetchuser, async (req, res) => {
+    const { title, description, tag } = req.body
+    // Create newNote Object
+   const newNote = {}
+    if (title) { newNote.title = title }
+    if (description) { newNote.description = description }
+    if (tag) { newNote.tag = tag }
+
+    // Find the note to be updated and upadate it
+    let notes = await Notes.findById(req.params.id)
+    if (!notes) {
+        return res.status(404).send("Not found")
+    }
+
+    // Check the user is same
+    if (notes.user.toString() !== req.user.id) {
+        return res.status(401).send("Not Allowed")
+    }
+
+    notes = await Notes.findByIdAndUpdate(req.params.id, { $set: newNote }, { new: true })
+    res.json(notes)
+})
+
+
 module.exports = router;
